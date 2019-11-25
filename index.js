b914934c3c58fe3e359d4ad850babba38bd036ed
alexnightowl <alexeykolesnik7@gmail.com>
2019-11-25 14:27:20 +0200

    add gitignore

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ const fs = require('fs')
 const path = require('path')
 
 const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-type': 'text/html'
+    })
+    if (req.url === '/contact') {
+        res.end('<h1>Contact</h1>')
+    } else if (req.url === '/') {
+        res.end('<h1>Home</h1>')
+    }
+})
 
-            if (req.url === '/') {
-                res.end(path.join(__dirname, 'public', 'index.html'), (err, data) => {
-                        if (err) {
-                            throw err
-                        }
-                        res.writeHead(200, {
-                            'Content-Type': 'text/html'
-                        })
-                    }
-                }
-            })
-
-
-        server.listen(3000, () => { console.log('Server is working...') }
+server.listen(3000, () => {
+    console.log('Server is running...')
+})
