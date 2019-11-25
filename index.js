212be735d30452d203d5272b6f9a5e86bf45b4ca
alexnightowl <alexeykolesnik7@gmail.com>
2019-11-25 15:46:19 +0200

    create error page and refactoring

--- index.js
+++ index.js
@@ -3,14 +3,34 @@ const fs = require('fs')
 const path = require('path')
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-type': 'text/html'
-    })
-    if (req.url === '/contact') {
-        res.end('<h1>Contact</h1>')
-    } else if (req.url === '/') {
-        res.end('<h1>Home</h1>')
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+    const ext = path.extname(filePath)
+    if (!ext) {
+        filePath += '.html'
     }
+
+    console.log(filePath)
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+                if (err) {
+                    res.writeHead(500)
+                    res.end('Error')
+                } else {
+                    res.writeHead(200, {
+                        'Content-type': 'index.html'
+                    })
+                    res.end(data)
+                }
+            })
+        } else {
+            res.writeHead(200, {
+                'Content-type': 'index.html'
+            })
+            res.end(content)
+        }
+    })
 })
 
 server.listen(3000, () => {
